6c6cdfcf7c81a8ed545c851ffe771021664c482e
serushpmn <soroushpey@gmail.com>
2023-03-05 18:43:16 +0330

    do some work on side bar and script for open/close sidebar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const links = document.querySelectorAll(".links");
+const closeMenu = document.querySelector(".close-menu");
+const sideBar = document.querySelector(".side-bar");
 const expandArrow = document.querySelectorAll(".expand");
+
 links.forEach((link) => {
   link.addEventListener("click", () => {
     console.log("clicked");
@@ -14,4 +17,18 @@ links.forEach((link) => {
       subMenu.style.maxHeight = subMenu.scrollHeight + "px";
     }
   });
+});
+closeMenu.addEventListener("click", () => {
+  if (sideBar.classList.contains("closed")) {
+    sideBar.classList.remove("closed");
+    document.getElementById("main").style.width = "calc(100% - 28rem)";
+    document.getElementById("header").style.width = "calc(100% - 28rem)";
+  } else {
+    document.getElementById("main").style.width = "calc(100% - 4rem)";
+    document.getElementById("header").style.width = "calc(100% - 4rem)";
+    sideBar.classList.add("closed");
+  }
+
+  // document.getElementById("main").style.width = "calc(100% - 4rem)";
+  console.log("close button clicked");
 });
