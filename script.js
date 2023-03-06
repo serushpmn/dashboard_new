0453d1040945d85d1110ea53028c9f64eb46920c
serushpmn <soroushpey@gmail.com>
2023-03-06 20:10:10 +0330

    work on side bar in mobile size

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const links = document.querySelectorAll(".links");
 const closeMenu = document.querySelector(".close-menu");
+const closeMenuMobile = document.querySelector(".close-menu-mobile");
 const sideBar = document.querySelector(".side-bar");
 const expandArrow = document.querySelectorAll(".expand");
+const sideNav = document.getElementById("side-nav");
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
@@ -18,17 +20,39 @@ links.forEach((link) => {
     }
   });
 });
+
+sideNav.addEventListener("mouseover", () => {
+  console.log("mouse hover on sidebar");
+  sideBar.classList.remove("closed");
+  document.getElementById("main").style.width = "calc(100% - 28rem)";
+  document.getElementById("header").style.width = "calc(100% - 28rem)";
+});
+
 closeMenu.addEventListener("click", () => {
   if (sideBar.classList.contains("closed")) {
     sideBar.classList.remove("closed");
     document.getElementById("main").style.width = "calc(100% - 28rem)";
     document.getElementById("header").style.width = "calc(100% - 28rem)";
   } else {
-    document.getElementById("main").style.width = "calc(100% - 4rem)";
-    document.getElementById("header").style.width = "calc(100% - 4rem)";
+    document.getElementById("main").style.width = "calc(100% - 8rem)";
+    document.getElementById("header").style.width = "calc(100% - 8rem)";
+    sideBar.classList.add("closed");
+  }
+  console.log("close button clicked");
+});
+closeMenuMobile.addEventListener("click", () => {
+  if (sideBar.classList.contains("closed")) {
+    sideBar.classList.remove("closed");
+    // document.getElementById("main").style.width = "100%";
+    // document.getElementById("header").style.width = "100%";
+  } else {
+    // document.getElementById("main").style.width = "calc(100% - 8rem)";
+    // document.getElementById("header").style.width = "calc(100% - 8rem)";
     sideBar.classList.add("closed");
   }
 
   // document.getElementById("main").style.width = "calc(100% - 4rem)";
-  console.log("close button clicked");
+  console.log("close mobile button clicked");
 });
+
+// .console.log(closeMenu.addEventListener("mouseOver"));
